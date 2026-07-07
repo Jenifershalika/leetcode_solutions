1e41efd7e861f47f8777bef29b7e37ff8c7ee63b
Jenifershalika <jenifershalika.s718@gmail.com>
2026-07-07 09:52:51 +0530

    Time: 42 ms (54.91%) | Memory: 19.9 MB (41.45%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,34 +1,33 @@
 class Solution:
-    def findAnagrams(self, s: str, p: str) -> List[int]:
-        
+    def findAnagrams(self, s: str, p: str):
         if len(p) > len(s):
             return []
 
-        p_count = [0] * 26
-        window = [0] * 26
+        p_count = {}
+        window = {}
 
-        # Store frequency of characters in p
+        # Count characters in p
         for ch in p:
-            index = ord(ch) - ord('a')
-            p_count[index] += 1
+            p_count[ch] = p_count.get(ch, 0) + 1
 
         left = 0
         ans = []
 
         for right in range(len(s)):
-            # Add current character to window
-            index = ord(s[right]) - ord('a')
-            window[index] += 1
+            # Add current character
+            window[s[right]] = window.get(s[right], 0) + 1
 
-            # If window becomes larger than p
+            # Keep window size equal to len(p)
             if right - left + 1 > len(p):
-                index = ord(s[left]) - ord('a')
-                window[index] -= 1
+                window[s[left]] -= 1
+
+                if window[s[left]] == 0:
+                    del window[s[left]]
+
                 left += 1
 
-            # Compare frequencies
+            # Check if current window is an anagram
             if window == p_count:
                 ans.append(left)
 
         return ans
-        
